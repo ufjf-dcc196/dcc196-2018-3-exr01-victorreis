22457186eae9a5a8f56fc333178231ed41e57447
victorreis <v.crisostomo.reis@gmail.com>
2018-09-12 18:03:14 -0300

    Em cada um dos IFs verifica-se se os dados foram recebidos, atualiza-se os respectivos contadores e exibe-se um TOAST informando a modificação.

--- app/src/main/java/br/ufjf/dcc196/dcc196_2018_3_exr01_victorreis/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/dcc196_2018_3_exr01_victorreis/MainActivity.java
@@ -12,21 +12,25 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnAlunos;
-    private Button btnServidores;
-    private Button btnExternos;
     private TextView txtNumeroAlunos;
     private TextView txtNumeroServidores;
     private TextView txtNumeroExternos;
-    public static final String ALUNO_NOME = "nome";
-    public static final String ALUNO_MATRICULA = "matricula";
-    public static final String SERVIDOR_NOME = "nome";
-    public static final String SERVIDOR_SIAPE = "siape";
-    public static final String EXTERNO_NOME = "nome";
-    public static final String EXTERNO_EMAIL = "email";
+    private TextView txtNumeroTotal;
+
+    private Button btnAlunos;
+    private Button btnServidores;
+    private Button btnExternos;
+
     private static final int REQUEST_ALUNO = 1;
     private static final int REQUEST_SERVIDOR = 2;
     private static final int REQUEST_EXTERNO = 3;
+
+    public static final String ALUNO_NOME = "ALUNO_NOME";
+    public static final String ALUNO_MATRICULA = "ALUNO_MATRICULA";
+    public static final String SERVIDOR_NOME = "SERVIDOR_NOME";
+    public static final String SERVIDOR_SIAPE = "SERVIDOR_SIAPE";
+    public static final String EXTERNO_NOME = "EXTERNO_NOME";
+    public static final String EXTERNO_EMAIL = "EXTERNO_EMAIL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         txtNumeroAlunos = (TextView) findViewById(R.id.txt_nro_alunos);
         txtNumeroServidores = (TextView) findViewById(R.id.txt_nro_servidores);
         txtNumeroExternos = (TextView) findViewById(R.id.txt_nro_externos);
+        txtNumeroTotal = (TextView) findViewById(R.id.txt_nro_total);
 
         btnAlunos = (Button) findViewById(R.id.btn_alunos);
         btnAlunos.setOnClickListener(new View.OnClickListener() {
@@ -69,13 +74,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == MainActivity.REQUEST_ALUNO && resultCode == Activity.RESULT_OK && data != null) {
-            Toast.makeText(getApplicationContext(), "Retornou de ALUNO",Toast.LENGTH_SHORT).show();
+            String nome = data.getExtras().getString(MainActivity.ALUNO_NOME);
+            String matricula = data.getExtras().getString(MainActivity.ALUNO_MATRICULA);
+
+            if (nome == null || matricula == null || nome.isEmpty() || matricula.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Favor preencher todos os campos.",Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Integer totalAlunos = Integer.parseInt(txtNumeroAlunos.getText().toString()) + 1;
+            Integer total = Integer.parseInt(txtNumeroTotal.getText().toString()) + 1;
+
+            txtNumeroAlunos.setText(String.valueOf(totalAlunos));
+            txtNumeroTotal.setText(String.valueOf(total));
+
+            Toast.makeText(getApplicationContext(), "ALUNO Cadastrado",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Nome: " + nome,Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Matricula: " + matricula,Toast.LENGTH_LONG).show();
 
         } else if (requestCode == MainActivity.REQUEST_SERVIDOR && resultCode == Activity.RESULT_OK && data != null) {
-            Toast.makeText(getApplicationContext(), "Retornou de SERVIDOR",Toast.LENGTH_SHORT).show();
+            String nome = data.getExtras().getString(MainActivity.SERVIDOR_NOME);
+            String siape = data.getExtras().getString(MainActivity.SERVIDOR_SIAPE);
+
+            if (nome == null || siape == null || nome.isEmpty() || siape.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Favor preencher todos os campos.",Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Integer totalServidores = Integer.parseInt(txtNumeroServidores.getText().toString()) + 1;
+            Integer total = Integer.parseInt(txtNumeroTotal.getText().toString()) + 1;
+
+            txtNumeroServidores.setText(String.valueOf(totalServidores));
+            txtNumeroTotal.setText(String.valueOf(total));
+
+            Toast.makeText(getApplicationContext(), "SERVIDOR Cadastrado",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Nome: " + nome,Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Siape: " + siape,Toast.LENGTH_LONG).show();
 
         } else if (requestCode == MainActivity.REQUEST_EXTERNO && resultCode == Activity.RESULT_OK && data != null) {
-            Toast.makeText(getApplicationContext(), "Retornou de EXTERNO",Toast.LENGTH_SHORT).show();
+            String nome = data.getExtras().getString(MainActivity.EXTERNO_NOME);
+            String email = data.getExtras().getString(MainActivity.EXTERNO_EMAIL);
+
+            if (nome == null || email == null || nome.isEmpty() || email.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Favor preencher todos os campos.",Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Integer totalExternos = Integer.parseInt(txtNumeroExternos.getText().toString()) + 1;
+            Integer total = Integer.parseInt(txtNumeroTotal.getText().toString()) + 1;
+
+            txtNumeroExternos.setText(String.valueOf(totalExternos));
+            txtNumeroTotal.setText(String.valueOf(total));
+
+            Toast.makeText(getApplicationContext(), "SERVIDOR Cadastrado",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Nome: " + nome,Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "E-mail: " + email,Toast.LENGTH_LONG).show();
         }
     }
 }
