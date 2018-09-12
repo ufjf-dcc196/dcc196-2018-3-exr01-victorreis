13b1569f7bcd9e60bcc0ab59a6eb713a238f6b7b
victorreis <v.crisostomo.reis@gmail.com>
2018-09-12 17:07:09 -0300

    Adicionadas as variáveis "final static" referentes as informações e as REQUESTS em MainActivity.
    Layout de AlunoActivity feito.
    
    Atualizado o Layout de MainActivity.
    
    Retornando o resultado de AlunoActivity para MainActivity.

--- app/src/main/java/br/ufjf/dcc196/dcc196_2018_3_exr01_victorreis/AlunoActivity.java
+++ app/src/main/java/br/ufjf/dcc196/dcc196_2018_3_exr01_victorreis/AlunoActivity.java
@@ -1,13 +1,40 @@
 package br.ufjf.dcc196.dcc196_2018_3_exr01_victorreis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AlunoActivity extends AppCompatActivity {
+
+    private EditText edtAlunoNome;
+    private EditText edtAlunoMatricula;
+    private Button btnAlunoNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aluno);
+
+        edtAlunoNome = (EditText) findViewById(R.id.edt_aluno_nome);
+        edtAlunoMatricula = (EditText) findViewById(R.id.edt_aluno_matricula);
+
+        btnAlunoNome = (Button) findViewById(R.id.btn_cadastrar);
+        btnAlunoNome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultado = new Intent();
+
+                String nome = edtAlunoNome.getText().toString();
+                String matricula = edtAlunoMatricula.getText().toString();
+                resultado.putExtra(MainActivity.ALUNO_NOME, nome);
+                resultado.putExtra(MainActivity.ALUNO_MATRICULA, matricula);
+
+                setResult(RESULT_OK, resultado);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/br/ufjf/dcc196/dcc196_2018_3_exr01_victorreis/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/dcc196_2018_3_exr01_victorreis/MainActivity.java
@@ -1,5 +1,6 @@
 package br.ufjf.dcc196.dcc196_2018_3_exr01_victorreis;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,44 +17,58 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtNumeroAlunos;
     private TextView txtNumeroServidores;
     private TextView txtNumeroExternos;
-//    public static final String PESSOA_NOME = "nome";
-    private static final int REQUEST_EXEMPLO = 1;
+    public static final String ALUNO_NOME = "nome";
+    public static final String ALUNO_MATRICULA = "matricula";
+    public static final String SERVIDOR_NOME = "nome";
+    public static final String SERVIDOR_SIAPE = "siape";
+    public static final String EXTERNO_NOME = "nome";
+    public static final String EXTERNO_EMAIL = "email";
+    private static final int REQUEST_ALUNO = 1;
+    private static final int REQUEST_SERVIDOR = 2;
+    private static final int REQUEST_EXTERNO = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtNumeroAlunos = (TextView) findViewById(R.id.txt_numero_alunos);
-        txtNumeroServidores = (TextView) findViewById(R.id.txt_numero_servidores);
-        txtNumeroExternos = (TextView) findViewById(R.id.txt_numero_externos);
+        txtNumeroAlunos = (TextView) findViewById(R.id.txt_nro_alunos);
+        txtNumeroServidores = (TextView) findViewById(R.id.txt_nro_servidores);
+        txtNumeroExternos = (TextView) findViewById(R.id.txt_nro_externos);
 
         btnAlunos = (Button) findViewById(R.id.btn_alunos);
         btnAlunos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AlunoActivity.class);
-//                intent.putExtra("nome", edtNome.getText().toString());
-                startActivityForResult(intent, MainActivity.REQUEST_EXEMPLO);
+                startActivityForResult(intent, MainActivity.REQUEST_ALUNO);
             }
         });
+
         btnServidores = (Button) findViewById(R.id.btn_servidores);
         btnServidores.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ServidorActivity.class);
-//                intent.putExtra("nome", edtNome.getText().toString());
-                startActivityForResult(intent, MainActivity.REQUEST_EXEMPLO);
+                startActivityForResult(intent, MainActivity.REQUEST_SERVIDOR);
             }
         });
+
         btnExternos = (Button) findViewById(R.id.btn_externos);
         btnExternos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ExternoActivity.class);
-//                intent.putExtra("nome", edtNome.getText().toString());
-                startActivityForResult(intent, MainActivity.REQUEST_EXEMPLO);
+                startActivityForResult(intent, MainActivity.REQUEST_EXTERNO);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MainActivity.REQUEST_ALUNO && resultCode == Activity.RESULT_OK && data != null) {
+
+        }
     }
 }
